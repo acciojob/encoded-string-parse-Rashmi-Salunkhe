036fe2446f270749b29672790d9eda08a07c7391
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-08-27 13:44:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const parseCode = (str) => {
-  // your code here
+    // Split the string by '0' and filter out any empty strings
+    const parts = str.split('0').filter(Boolean);
+
+    // Return an object with the parsed values
+    return {
+        firstName: parts[0],
+        lastName: parts[1],
+        id: parts[2]
+    };
 };
 
 // Do not change the code below
